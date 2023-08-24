6b9e602e2f8d07be731bb55dce3e70791b966de8
CALIEW <caliew888@gmail.com>
2023-08-24 21:12:49 +0800

    Add new Sensor for Shinko

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -86,61 +86,64 @@ const Navbar = (props) => {
       onClick={()=>toggleCollapse('mainNavbarCollapse')}
     />
   );
+  const navbarLayout = (
+    <MDBNavbar color='indigo' dark expand='md' fixed='top' scrolling>
+      <MDBNavbarBrand href='/' className='py-0 font-weight-light'>
+        {
+          user &&  (user.companyname === "AeroSOFT Technologies Pte Ltd" || user.companyname === "TDK" || 
+                    user.companyname === "SuaraKOM" ) ? (
+            <a class="navbar-brand" href="/en">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 850.4 178.6" height="25px" preserveAspectRatio="none" fill={'white'}>
+            <polygon points="103.1,37.8 65.3,75.6 140.8,75.6 "></polygon>
+            <polygon points="103.1,140.8 154.6,127 140.8,75.5 "></polygon>
+            <polygon points="154.6,127 168.4,178.6 206.2,113.2 "></polygon>
+            <polygon points="0,113.2 37.7,178.6 51.5,127 "></polygon>
+            <polygon points="51.5,127 103.1,140.8 65.3,75.5 "></polygon>
+            <polygon points="65,0 -0.3,113.2 65,75.5 "></polygon>
+            <polygon points="206.4,113.2 141,0 141,75.5 "></polygon>
+            <polygon points="37.7,178.6 168.4,178.6 103.1,140.8 "></polygon>
+            <polygon points="140.8,-0.4 65.3,-0.4 103.1,37.3 "></polygon>
+            <polygon points="281.7,38 281.7,178.5 330.3,178.5 330.3,38 411,38 411,0 201,0 201,38 "></polygon>
+            <path d="M530.2,0H420.5v178.5h109.7c70.3,0,100.4-49.1,100.4-89.2C630.6,49.1,600.5,0,530.2,0z M539.3,139.8h-72.2V37.9
+            h72.2c19.3,0,46.1,20.5,46.1,51.4C585.4,119.4,558.4,139.8,539.3,139.8z"></path>
+            <rect x="638.9" width="48.7" height="178.5"></rect>
+            <polygon points="689,85.3 780.7,0 845.2,0 749.1,85.3 850.4,178.5 783,178.5 "></polygon>
+            </svg>
+            {/* <span>TDK Electronics · TDK Europe</span> */}
+            <span> TDK ELECTRONICS MALAYSIA </span>
+            </a>                          
+          ) : (
+            <>
+            <MDBIcon icon="yin-yang" size="1x"/>
+            <strong className='px-md-3 align-middle'>{ user ? user.companyname : 'IOT PORTAL'}</strong>
+            </>
+          )
+        }
+      </MDBNavbarBrand>
+      <MDBNavbarToggler onClick={()=>toggleCollapse('mainNavbarCollapse')} />
+
+      <MDBCollapse id='mainNavbarCollapse' isOpen={collapseID} navbar>
+        {isAuthenticated ? authLinks : guestLinks}
+      </MDBCollapse>
+
+    </MDBNavbar>
+
+  )
   // ----- viewBox="0 0 850.4 178.6"
   return (
     <div className='flyout'>
-        <MDBNavbar color='indigo' dark expand='md' fixed='top' scrolling>
-          <MDBNavbarBrand href='/' className='py-0 font-weight-light'>
-            {
-              user &&  (user.companyname === "AeroSOFT Technologies Pte Ltd" || user.companyname === "TDK" || 
-                        user.companyname === "SuaraKOM" ) ? (
-                <a class="navbar-brand" href="/en">
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 850.4 178.6" height="25px" preserveAspectRatio="none" fill={'white'}>
-                <polygon points="103.1,37.8 65.3,75.6 140.8,75.6 "></polygon>
-                <polygon points="103.1,140.8 154.6,127 140.8,75.5 "></polygon>
-                <polygon points="154.6,127 168.4,178.6 206.2,113.2 "></polygon>
-                <polygon points="0,113.2 37.7,178.6 51.5,127 "></polygon>
-                <polygon points="51.5,127 103.1,140.8 65.3,75.5 "></polygon>
-                <polygon points="65,0 -0.3,113.2 65,75.5 "></polygon>
-                <polygon points="206.4,113.2 141,0 141,75.5 "></polygon>
-                <polygon points="37.7,178.6 168.4,178.6 103.1,140.8 "></polygon>
-                <polygon points="140.8,-0.4 65.3,-0.4 103.1,37.3 "></polygon>
-                <polygon points="281.7,38 281.7,178.5 330.3,178.5 330.3,38 411,38 411,0 201,0 201,38 "></polygon>
-                <path d="M530.2,0H420.5v178.5h109.7c70.3,0,100.4-49.1,100.4-89.2C630.6,49.1,600.5,0,530.2,0z M539.3,139.8h-72.2V37.9
-                h72.2c19.3,0,46.1,20.5,46.1,51.4C585.4,119.4,558.4,139.8,539.3,139.8z"></path>
-                <rect x="638.9" width="48.7" height="178.5"></rect>
-                <polygon points="689,85.3 780.7,0 845.2,0 749.1,85.3 850.4,178.5 783,178.5 "></polygon>
-                </svg>
-                {/* <span>TDK Electronics · TDK Europe</span> */}
-                <span> TDK ELECTRONICS MALAYSIA </span>
-                </a>                          
-              ) : (
-                <>
-                <MDBIcon icon="yin-yang" size="1x"/>
-                <strong className='px-md-3 align-middle'>{ user ? user.companyname : 'IOT PORTAL'}</strong>
-                </>
-              )
-            }
-          </MDBNavbarBrand>
-          <MDBNavbarToggler onClick={()=>toggleCollapse('mainNavbarCollapse')} />
-
-          <MDBCollapse id='mainNavbarCollapse' isOpen={collapseID} navbar>
-            {isAuthenticated ? authLinks : guestLinks}
-          </MDBCollapse>
-
-        </MDBNavbar>
-
-        {collapseID && overlay}
-        <main style={{ marginTop: '6rem' }}>
-          <Routes />
-        </main>
-        
-        <MDBFooter color='indigo'>
-          <p className='footer-copyright mb-0 py-3 text-center'>
-            &copy; {new Date().getFullYear()} Copyright:
-            <a href='https://www.MDBootstrap.com'> AeroSOFT Technologies Pte Ltd </a>
-          </p>
-        </MDBFooter>  
+      { true && navbarLayout }
+      {collapseID && overlay}
+      <main style={{ marginTop: '6rem' }}>
+        <Routes />
+      </main>
+      
+      <MDBFooter color='indigo'>
+        <p className='footer-copyright mb-0 py-3 text-center'>
+          &copy; {new Date().getFullYear()} Copyright:
+          <a href='https://www.MDBootstrap.com'> AeroSOFT Technologies Pte Ltd </a>
+        </p>
+      </MDBFooter>  
     </div>
   )
 }
--- client/src/components/pages/OverView.js
+++ client/src/components/pages/OverView.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect,useState,useContext } from 'react';
 import axios from 'axios';
 import AuthContext from '../../context/auth/authContext';
