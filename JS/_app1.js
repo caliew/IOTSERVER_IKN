f9c4e5097250177607cdf579eaf06f7d75d5a913
caliew <caliew888@gmail.com>
2025-06-07 11:56:08 +0800

    Update of _App1 with external json file

--- JS/_app1.js
+++ JS/_app1.js
@@ -6,117 +6,94 @@ const currentDirectory = process.cwd(); // Get the current working directory
 // Declare the app
 const app = {};
 const fileData = {}; // Initialize an empty object to store file data
-// ------------------
-const splitDate = '2023-02-13';
-const filePath = 'B0-68-61-5E-F9-72.log';
-// -------------
-// Init function
-app.writeObjectToFile = function(_arrayOfObjects,_outputFilePath,index) {
-  if (index >= _arrayOfObjects.length) {
-    console.log('All objects have been written to', _outputFilePath);
-    return;
-  }
-  const jsonObject = _arrayOfObjects[index];
-  const jsonObjectString = JSON.stringify(jsonObject); // The `null` and `2` arguments are for formatting (indentation)
-  // Append a comma and newline for all but the last object
-  const separator = index === _arrayOfObjects.length - 1 ? '' : '\n';
-  fs.appendFile(_outputFilePath, jsonObjectString + separator, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-    } else {
-      app.writeObjectToFile(_arrayOfObjects,_outputFilePath,index + 1,); // Write the next object
-    }
-  });
-}
-// -------------------
-app.extract = function (splitDate) {
-  // --------
-  let _splitDate = new Date(splitDate);
-  console.log('.. . . . . . ..');
-  console.log('.. UTILITIES ..');
-  console.log('.. DIRECTORY \t',currentDirectory);
-  console.log('.. SPLIT DATE \t',_splitDate)
-  // ---------------------------------------------
-  // READ CURRENT DIRECTORY GET DIRECTORY FILE MAP
-  // ---------------------------------------------
-  fs.readdir(currentDirectory, (err, files) => {
-    if (err) {
-      console.error('Error reading directory:', err);
-      return;
-    }  
-    files.forEach((file) => {
-      fs.stat(file, (err, stats) => {
-        if (err) {
-          console.error('Error getting file stats:', err);
-          return;
-        };
-        fileData[file] = stats.size;
-        // Check if all files have been processed and log the result
-        if (Object.keys(fileData).length === files.length) {
-          console.log(`.. TOTAL FILES IN DIRECTORY : <${Object.keys(fileData).length}>`);
+
+// Load configuration from _app1.json
+const configFilePath = '_app1.json';
+const configFileContent = fs.readFileSync(configFilePath, 'utf8');
+const config = JSON.parse(configFileContent);
+
+// Function to extract data from files
+app.extract = function() {
+  const filePaths = config.filesnames.map(filename => path.join(config.path, `${filename}${config.fileExtension}`));
+
+  filePaths.forEach(filePath => {
+    // READ FILE TO DO SPLIT
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err);
+        return;
+      }
+
+      // Convert data to array of objects
+      const lines = data.trim().split('\n');
+      const fileDataObjects = lines.map(line => {
+        try {
+          return JSON.parse(line);
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
+          return null;
         }
       });
+
+      // Split data into BEF and AFT
+      const _splitDate = new Date(config.splitDate);
+      const objArr1 = [];
+      const objArr2 = [];
+      fileDataObjects.forEach(object => {
+        if (object) {
+          const dateTime = new Date(object.TIMESTAMP);
+          if (dateTime < _splitDate) {
+            objArr1.push(object);
+          } else {
+            objArr2.push(object);
+          }
+        }
+      });
+
+      // Create output file paths
+      const outputFilePath1 = `${filePath}_BEF.log`;
+      const outputFilePath2 = `${filePath}_AFT.log`;
+
+      // Write data to output files
+      app.appendFile(objArr1, outputFilePath1);
+      app.appendFile(objArr2, outputFilePath2);
     });
-  });
-  // ---------------------
-  // READ FILE TO DO SPLIT
-  // ---------------------
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Error reading file:', err);
-      return;
-    }
-    // -----------------------------------
-    const lines = data.trim().split('\n');
-    let objArr1 = [];
-    let objArr2 = [];
-    // --------------
-    const fileDataObjects = lines.map((line) => {
-      try {
-        let JSONObject = JSON.parse(line)
-        let dateTime = new Date(JSONObject.TIMESTAMP);
-        if (dateTime < _splitDate) objArr1.push(JSONObject)
-        else objArr2.push(JSONObject);
-        return JSONObject;
-      } catch (error) {
-        console.error('Error parsing JSON:', error);
-        return null;
-      }
-    });
-    // ------------------------------
-    let Object1 = fileDataObjects[0];
-    let Object2 = fileDataObjects[fileDataObjects.length-1];
-    let dateTime1 = new Date(Object1.TIMESTAMP);
-    let dateTime2 = new Date(Object2.TIMESTAMP);
-    // -----------------------------------------
-    console.log('  ..LOG FILE READ : \t',filePath);
-    console.log(`  ..[${dateTime1.toLocaleDateString()}]|MONTH[${dateTime1.getMonth()+1}] > [${dateTime2.toLocaleDateString()}]|MONTH[${dateTime2.getMonth()+1}]`)
-    console.log(`  ..ARRAY1 [${objArr1.length}] ..ARRAY2 [${objArr2.length}]`);
-    // Convert the array of objects to a JSON string
-    const jsonArrayString1 = JSON.stringify(objArr1, null, 2); // The `null` and `2` arguments are for formatting (indentation)
-    const jsonArrayString2 = JSON.stringify(objArr2, null, 2); // The `null` and `2` arguments are for formatting (indentation)
-    // Specify the path for the output JSON file
-    const outputFilePath1 = '_splitBEF.log'; // Replace with your desired output file path
-    const outputFilePath2 = '_splitAFT.log'; // Replace with your desired output file path
-    // ---------------------------------
-    // Write the JSON string to the file
-    app.appendFile(objArr1,outputFilePath1);
-    app.appendFile(objArr2,outputFilePath2);
-    // --------    
   });
 };
-app.appendFile = function(_arrayOfObjects,_outputFilePath) {
+
+// Function to append data to file
+app.appendFile = function(_arrayOfObjects, _outputFilePath) {
   fs.writeFile(_outputFilePath, '', 'utf8', (err) => {
     if (err) {
       console.error('Error writing to file:', err);
     } else {
-      app.writeObjectToFile(_arrayOfObjects,_outputFilePath,0); // Start writing objects
+      app.writeObjectToFile(_arrayOfObjects, _outputFilePath, 0); // Start writing objects
     }
-  });  
-}
-// -----
-// Self executing
-app.extract(splitDate);
+  });
+};
+
+// Function to write object to file
+app.writeObjectToFile = function(_arrayOfObjects, _outputFilePath, index) {
+  if (index >= _arrayOfObjects.length) {
+    console.log('All objects have been written to', _outputFilePath);
+    return;
+  }
+
+  const jsonObject = _arrayOfObjects[index];
+  const jsonObjectString = JSON.stringify(jsonObject, null, 2); // The `null` and `2` arguments are for formatting (indentation)
+  const separator = index === _arrayOfObjects.length - 1 ? '' : '\n';
+
+  fs.appendFile(_outputFilePath, jsonObjectString + separator, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      app.writeObjectToFile(_arrayOfObjects, _outputFilePath, index + 1); // Write the next object
+    }
+  });
+};
+
+// Self-executing function
+app.extract();
 
 // Export the app
 module.exports = app;
